7a850e69ba440b4fc89a1be1aab563bdadb9f0b2
0x-sudo <rahul_chaudhary@tutanota.com>
2024-03-03 19:21:34 +0530

    .

--- app/routes/_auth.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "@remix-run/react";
-
-export default function AuthLayout() {
-  return (
-    <div className="max-w-4xl mx-auto p-6 w-full h-screen flex items-center justify-center">
-      <Outlet />
-    </div>
-  );
-}
