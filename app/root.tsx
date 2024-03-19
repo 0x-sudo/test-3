58315c31499ac3c47cc465a64eb0b31f468e098b
0x-sudo <rahul_chaudhary@tutanota.com>
2024-03-20 02:45:11 +0530

    🍻 Added tailwindcss, fonts & other minor changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,38 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useNavigate,
+} from '@remix-run/react'
+import './styles/fonts.css'
+import './styles/globals.css'
+import { RouterProvider } from 'react-aria-components'
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="dark">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="font-sans">
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	)
 }
 
 export default function App() {
-  return <Outlet />;
+	const navigate = useNavigate()
+	return (
+		<RouterProvider navigate={navigate}>
+			<Outlet />
+		</RouterProvider>
+	)
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex h-screen w-full flex-col p-6 lg:px-0">
+      <div className="flex flex-col max-w-sm">
+        <h1 className="text-4xl font-extrabold mb-3 text-primary">Learning about Remix while building Cosmos</h1>
+      </div>
+      <MainNav />
     </div>
-  );
+  )
+}
+
+export const MainNav = () => {
+  return (
+    <div className="border border-border p-2 rounded-full absolute bottom-6 max-w-md mx-auto w-full left-0 right-0">
+      <div className="text-lg font-medium rounded-full bg-secondary w-fit p-2">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-secondary-foreground">
+          <path opacity="0.14" d="M3 19L12 7L21 19V21H16L12 15L8 21H3V19Z" fill="currentColor" />
+          <path d="M15 3L3 19V21H21V19L9 3M12 15L16 21H8L12 15Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+      </div>
+    </div>
+  )
 }
